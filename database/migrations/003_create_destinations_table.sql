404b99d66e7704f2e2c8a8cb291ab5677ce30a7e
OgataiKhan <badellino.kevin@gmail.com>
2024-08-19 16:28:56 +0200

    feat: add is visited to destinations

--- database/migrations/003_create_destinations_table.sql
+++ database/migrations/003_create_destinations_table.sql
@@ -5,6 +5,7 @@ CREATE TABLE destinations (
     latitude DOUBLE,
     longitude DOUBLE,
     description TEXT,
+    is_visited BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (day_id) REFERENCES days(id)
 );
