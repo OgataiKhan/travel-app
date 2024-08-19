b57b07c70af14374047352aa170ef69bbb7308f0
OgataiKhan <badellino.kevin@gmail.com>
2024-08-19 16:12:40 +0200

    feat: pivot table updated at

--- database/migrations/005_create_destination_ratings_table.sql
+++ database/migrations/005_create_destination_ratings_table.sql
@@ -3,6 +3,7 @@ CREATE TABLE destination_ratings (
     destination_id INT NOT NULL,
     rating_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (destination_id) REFERENCES destinations(id) ON DELETE CASCADE,
     FOREIGN KEY (rating_id) REFERENCES ratings(id) ON DELETE CASCADE
 );
