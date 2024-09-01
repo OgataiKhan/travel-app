11303321d88f01c6eb9f1ae14676fbd32ac246b8
OgataiKhan <badellino.kevin@gmail.com>
2024-09-01 13:24:16 +0200

    feat: php env support

--- backend/api/headers.php
+++ backend/api/headers.php
@@ -1,13 +1,28 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
+
+// Load environment variables from .env file
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+// try {
+//     $dotenv->load();
+//     echo "Environment file loaded successfully.<br>";
+// } catch (Exception $e) {
+//     echo "Failed to load environment file: " . $e->getMessage() . "<br>";
+// }
+
+$allowed_origin = $_ENV['ALLOWED_ORIGIN'];
+
 // Set CORS headers
-header('Access-Control-Allow-Origin: http://localhost:5173');
+header("Access-Control-Allow-Origin: $allowed_origin");
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 
 // Handle pre-flight OPTIONS request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 
-    header('Access-Control-Allow-Origin: http://localhost:5173');
+    header("Access-Control-Allow-Origin: $allowed_origin");
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
     exit(0); 
