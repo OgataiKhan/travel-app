8e6072026476b5a2901f01c47840ab4d4018992e
OgataiKhan <badellino.kevin@gmail.com>
2024-09-01 14:03:51 +0200

    fix: vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    rollupOptions: {
-      // Specify modules that should be treated as external
-      external: [
-        '@tomtom-international/web-sdk-maps',
-        '@tomtom-international/web-sdk-services',
-        '@tomtom-international/web-sdk-plugin-searchbox',
-        '@tomtom-international/web-sdk-maps/dist/maps.css'
-      ],
-    },
-  },
 })
